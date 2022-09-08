be31dcbd3573b7e779dc1b0ed1e3d409ae333353
TDerig23 <thomasderig23@gmail.com>
2022-09-07 23:55:07 -0700

    initial commit

---
+++ bda602_hw1/new_scratch.py
@@ -0,0 +1,10 @@
+import pandas
+
+
+class DataLoader():
+    def __init__(self,filename):
+        print("Starting Data Loader")
+        self.data = pandas.read_csv(filename)
+
+    def __del__(self):
+        print("ending dataloader")
