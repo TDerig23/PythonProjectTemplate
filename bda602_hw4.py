c3eb6615683bcbe42a985fc58a9e50fdf983e018
TDerig23 <89554971+TDerig23@users.noreply.github.com>
2022-10-15 00:04:28 -0700

    hw4_final
    unless I add another before you download it

--- bda602_hw4.py
+++ bda602_hw4.py
@@ -1,55 +1,60 @@
+from io import StringIO
+
 import pandas as pd
 import sys
 import numpy
+import statsmodels as statsmodels
 from plotly import express as px
 from plotly import figure_factory as ff
 from plotly import graph_objects as go
 from sklearn.metrics import confusion_matrix
-
-titanic_df = pd.read_csv(
-    "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/titanic.csv")
-
-
-## this is where I will separate the column of an inputted dataset into categorical and continious.
-def columnsep():
+from sklearn.model_selection import GridSearchCV
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
+import random
 
 
-cat_df = []
-cont_df = []
-for col in titanic_df:
-    if titanic_df[col].dtypes == "object" or titanic_df[col].dtypes == "bool":
-        extracted_cat = titanic_df[col]
-        cat_df = cat_df.append(extracted_cat)
-    else:
-        extracted_cont = titanic_df[col]
-        cont_df = cont_df.append(extracted_cont)
+def column_sep(dataframe):
+    cat_df = pd.DataFrame()
+    cont_df = pd.DataFrame()
+    for col in dataframe:
+        if dataframe[col].dtypes == "bool" or dataframe[col].dtypes == "object" or len(pd.unique(dataframe[col])) == 2:
+            extracted_cat = dataframe[col]
+            cat_df = cat_df.append(extracted_cat)
+        else:
+            extracted_cont = dataframe[col]
+            cont_df = cont_df.append(extracted_cont)
+
+    return cat_df, cont_df
 
 
-def cont_resp_cat_predictor(dataframe, cat_predictor, labels):
+def get_column_names(dataframe):
+    cat_df, cont_df = column_sep(dataframe)
+
+    return cat_df.info, cont_df.info
+
+
+def cont_resp_cat_predictor(dataframe):
+    cat_df, cont_df = column_sep(dataframe)
+    n = random.randint(0, 8)
     # Add histogram data
-
+    hist_data = cat_df.iloc[:, n]
+    group_labels = cont_df.iloc[:, n].values
     # Group data together
 
-    group_labels = dataframe[cat_predictor].values
-
     # Create distribution plot with custom bin_size
-    fig_1 = ff.create_distplot(dataframe, group_labels, bin_size=0.2)
+    fig_1 = ff.create_distplot(hist_data, group_labels, bin_size=0.2)
     fig_1.update_layout(
         title="Continuous Response by Categorical Predictor",
         xaxis_title="Response",
         yaxis_title="Distribution",
     )
     fig_1.show()
-    fig_1.write_html(
-        file="../../../plots/lecture_6_cont_response_cat_predictor_dist_plot.html",
-        include_plotlyjs="cdn",
-    )
 
     fig_2 = go.Figure()
-    for curr_hist, curr_group in zip(dataframe, group_labels):
+    for curr_hist, curr_group in zip(hist_data, group_labels):
         fig_2.add_trace(
             go.Violin(
-                x=numpy.repeat(curr_group),
+                x=numpy.repeat(curr_group, n),
                 y=dataframe,
                 name=curr_group,
                 box_visible=True,
@@ -63,14 +68,16 @@ def cont_resp_cat_predictor(dataframe, cat_predictor, labels):
     )
     fig_2.show()
 
-    return
+    return fig_1, fig_2
 
 
-def cat_resp_cont_predictor(dataframe, cont_predictor):
+def cat_resp_cont_predictor(dataframe):
+    cat_df, cont_df = column_sep(dataframe)
+    n = random.randint(0, 8)
+    # Add histogram data
+    hist_data = cont_df.iloc[:, n]
+    group_labels = cat_df.iloc[:, n].values
     # Group data together
-    hist_data = [x1,
-
-                 group_labels = ["Response = 0", "Response = 1"]
 
     # Create distribution plot with custom bin_size
     fig_1 = ff.create_distplot(hist_data, group_labels, bin_size=0.2)
@@ -109,8 +116,15 @@ def cat_resp_cont_predictor(dataframe, cont_predictor):
     return
 
 
-def cat_response_cat_predictor(dataframe, cat_predictor):
-    conf_matrix = confusion_matrix(x_2, y_2)
+def cat_response_cat_predictor(dataframe):
+    cat_df, cont_df = column_sep(dataframe)
+    n = random.randint(0, 8)
+    # Add histogram data
+    x = cat_df.iloc[:, n].values
+    y = cont_df.iloc[:, n].values
+    # Group data together
+
+    conf_matrix = confusion_matrix(x, y)
 
     fig_no_relationship = go.Figure(
         data=go.Heatmap(z=conf_matrix, zmin=0, zmax=conf_matrix.max())
@@ -126,13 +140,7 @@ def cat_response_cat_predictor(dataframe, cat_predictor):
         include_plotlyjs="cdn",
     )
 
-    x = numpy.random.randn(n)
-    y = x + numpy.random.randn(n)
-
-    x_2 = [1 if abs(x_) > 1.5 else 0 for x_ in x]
-    y_2 = [1 if abs(y_) > 1.5 else 0 for y_ in y]
-
-    conf_matrix = confusion_matrix(x_2, y_2)
+    conf_matrix = confusion_matrix(x,y)
 
     fig_no_relationship = go.Figure(
         data=go.Heatmap(z=conf_matrix, zmin=0, zmax=conf_matrix.max())
@@ -150,7 +158,14 @@ def cat_response_cat_predictor(dataframe, cat_predictor):
     return
 
 
-def cont_response_cont_predictor(dataframe, cont_predictor):
+def cont_response_cont_predictor(dataframe):
+    cat_df, cont_df = column_sep(dataframe)
+    n = random.randint(0, 8)
+    # Add histogram data
+    x = cat_df.iloc[:, n]
+    y = cont_df.iloc[:, n].values
+    # Group data together
+
     fig = px.scatter(x=x, y=y, trendline="ols")
     fig.update_layout(
         title="Continuous Response by Continuous Predictor",
@@ -166,14 +181,18 @@ def cont_response_cont_predictor(dataframe, cont_predictor):
     return
 
 
-def datasetregression():
-    titanic_df = pd.read_csv(
-        "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/titanic.csv")
-    X = titanic_df.data
-    y = titanic_df.target
+def datasetregression(dataframe):
+    cat_df, cont_df = column_sep(dataframe)
+    n = random.randint(0, 8)
+    # Add histogram data
+    hist_data = cat_df.iloc[:, n]
+    group_labels = cont_df.iloc
+    # Group data together
+    X = dataframe.data
+    y = dataframe.target
 
     for idx, column in enumerate(X.T):
-        feature_name = diabetes.feature_names[idx]
+        feature_name = dataframe.feature_names[idx]
         predictor = statsmodels.api.add_constant(column)
         linear_regression_model = statsmodels.api.OLS(y, predictor)
         linear_regression_model_fitted = linear_regression_model.fit()
@@ -195,6 +214,119 @@ def datasetregression():
         fig.write_html(
             file=f"../../plots/lecture_6_var_{idx}.html", include_plotlyjs="cdn"
         )
+
+
+def plot_decision_tree(decision_tree, feature_names, class_names, file_out):
+    with StringIO() as dot_data:
+        export_graphviz(
+            decision_tree,
+            feature_names=feature_names,
+            class_names=class_names,
+            out_file=dot_data,
+            filled=True,
+        )
+        graph = pydot.graph_from_dot_data(dot_data.getvalue())
+
+
+def decision_tree_setup(dataframe):
+    # Increase pandas print viewport (so we see more on the screen)
+    pd.set_option("display.max_rows", 60)
+    pd.set_option("display.max_columns", 500)
+    pd.set_option("display.width", 1_000)
+
+    cat_df, cont_df = column_sep(dataframe)
+    n = random.randint(0, 8)
+    # Add histogram data
+
+    column_names = dataframe.columns
+
+    # Drop rows with missing values
+    dataframe = dataframe.dropna()
+
+    print("Original Dataset")
+
+    # Continuous Features
+
+    X = dataframe[cont_df].values
+
+    # Response
+    y = dataframe.iloc[:, n].values
+
+    # Decision Tree Classifier
+    max_tree_depth = 7
+    tree_random_state = 0  # Always set a seed
+    decision_tree = DecisionTreeClassifier(
+        max_depth=max_tree_depth, random_state=tree_random_state
+    )
+    decision_tree.fit(X, y)
+
+    # Plot the decision tree
+    plot_decision_tree(
+        decision_tree=decision_tree,
+        feature_names=x,
+        class_names="classification",
+        file_out="../../plots/lecture_6_iris_tree_full",
+    )
+
+    # Find an optimal tree via cross-validation
+    parameters = {
+        "max_depth": range(1, max_tree_depth),
+        "criterion": ["gini", "entropy"],
+    }
+    decision_tree_grid_search = GridSearchCV(
+        DecisionTreeClassifier(random_state=tree_random_state), parameters, n_jobs=4
+    )
+    decision_tree_grid_search.fit(X=X, y=y)
+
+    cv_results = DataFrame(decision_tree_grid_search.cv_results_["params"])
+    cv_results["score"] = decision_tree_grid_search.cv_results_["mean_test_score"]
+    print_heading("Cross validation results")
+    print(cv_results)
+    print_heading("Cross validation results - HTML table")
+    print(cv_results.to_html())
+
+    # Plot these cross_val results
+    gini_results = cv_results.loc[cv_results["criterion"] == "gini"]
+    entropy_results = cv_results.loc[cv_results["criterion"] == "entropy"]
+    data = [
+        go.Scatter(
+            x=gini_results["max_depth"].values,
+            y=gini_results["score"].values,
+            name="gini",
+            mode="lines",
+        ),
+        go.Scatter(
+            x=entropy_results["max_depth"].values,
+            y=entropy_results["score"].values,
+            name="entropy",
+            mode="lines",
+        ),
+    ]
+
+    layout = go.Layout(
+        title="Fisher's Iris Cross Validation",
+        xaxis_title="Tree Depth",
+        yaxis_title="Score",
+    )
+
+    fig = go.Figure(data=data, layout=layout)
+    fig.show()
+    fig.write_html(
+        file="../../plots/lecture_6_iris_cross_val.html",
+        include_plotlyjs="cdn",
+    )
+
+    # Get the "best" model
+    best_tree_model = decision_tree_grid_search.best_estimator_
+
+    # Plot this "best" decision tree
+    plot_decision_tree(
+        decision_tree=best_tree_model,
+        feature_names=continuous_features,
+        class_names="classification",
+        file_out="../../plots/lecture_6_iris_tree_cross_val",
+    )
+    return
 
 
 def main():
